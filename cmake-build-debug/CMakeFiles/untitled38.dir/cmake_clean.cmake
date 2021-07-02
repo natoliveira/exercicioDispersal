70499b904c5cf8c739ac7ef96d5f52126e3aae45
natoliveira <nataliadeolive@gmail.com>
2021-07-02 10:07:38 -0300

    implementacao final

---
+++ cmake-build-debug/CMakeFiles/untitled38.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled38.dir/exercicioDispersal.cpp.o"
+  "untitled38"
+  "untitled38.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled38.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
